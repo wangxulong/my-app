b6f123143e2dc3f193a81e2fd6e8d663d8da2fe6
wxl <duanxian0402@126.com>
2017-11-05 11:12:00 +0800

    商品信息组件和星级组件

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  public products: Array<Product>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.products = [
+      new Product(1, '第一个产品', 1.5, 4, ['电子产品', '食物'], '就是一个简单的介绍'),
+      new Product(2, '第2个产品', 2.5, 1, ['衣服'], '就是一个简单的介绍'),
+      new Product(3, '第3个产品', 3.5, 2, ['娱乐'], '就是一个简单的介绍'),
+      new Product(4, '第4个产品', 4.5, 3, ['韩妆品'], '就是一个简单的介绍'),
+      new Product(5, '第5个产品', 5.5, 4, ['电子产品', '其他'], '就是一个简单的介绍')
+    ];
+  }
+
+}
+
+export class Product {
+  constructor(public id: number,
+              public name: string,
+              public price: number,
+              public rating: number,
+              public categories: Array<string>,
+              public content: string) {
+  }
 }
